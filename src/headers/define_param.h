228597b813c191fa76384da9719fd369461da954
buw <bradwoord@gmail.com>
2023-06-07 12:08:30 +0500

    add const param#01

---
+++ src/headers/define_param.h
@@ -0,0 +1,8 @@
+//
+// Created by lowern1ght on 07.06.2023.
+//
+
+const string PARAM_NAME_COPY_TO   = "--p";
+const string PARAM_NAME_COPY_FROM = "--c";
+const string PARAM_NAME_LOG       = "--lg";
+const string PARAM_NAME_CHECK_ON  = "--chck";
